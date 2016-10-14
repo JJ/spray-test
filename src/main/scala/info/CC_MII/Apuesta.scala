ed89a04598b705d9b94bae760a08bb34f0405371
JJ Merelo <jjmerelo@gmail.com>
2016-10-14 07:58:49 +0200

    Probando tests en Scala

--- src/main/scala/info/CC_MII/Apuesta.scala
+++ src/main/scala/info/CC_MII/Apuesta.scala
@@ -1,6 +1,6 @@
 package info.CC_MII
 
-class Apuesta( var local:String, var visitante: String, var quien: String ) {
+class Apuesta( var local:Int, var visitante: Int, var quien: String ) {
   
-  override def toString = s"$quien: $local-$visitanted"
+  override def toString = s"$quien: $local-$visitante"
 }
